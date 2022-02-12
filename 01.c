38f8033bd7f94df11610cb8b1d37763744d5ee9c
WallysLS <wallys31.souza@gmail.com>
2022-02-11 23:45:54 -0300

    Created module

--- 01.c
+++ 01.c
@@ -1,19 +1,5 @@
-#include <stdio.h>
-#include <math.h>
+#include "module.h"
 
-int main()
-{
-    int valor;
-    int meses;
-    float montante;
-    float juros;
-    printf("Quanto você tem na poupança, em reais? ");
-    scanf("%d", &valor);
-    printf("Agora digite o número de meses que o dinheiro vai ficar guardado: ");
-    scanf("%d", &meses);
-    printf("You have %d reais na poupança hoje.", valor);
-    printf("\n");
-    juros = pow(1.01,meses);
-    montante = valor * juros;
-    printf("O montante no final de %i meses é de R$ %1f\n", meses, montante);
-}
+int soma(int a, int b){
+    printf("%d",a+b);
+};
---
+++ 02.c
@@ -0,0 +1,9 @@
+#include "module.h"
+#include <stdio.h>
+
+
+int main(void){
+
+soma(2,465);
+
+}
