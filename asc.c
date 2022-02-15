8bec601fa1572651d7b67e4ee9b7a5dba0d15d33
WallysLS <wallys31.souza@gmail.com>
2022-02-15 18:32:40 -0300

    Updated

--- asc.c
+++ asc.c
@@ -2,11 +2,18 @@
 int main(void)
 {
 	
-    int number = 65;
-    int *pointer;
+int a;
+int b = 4;
+int *c;
+c = &b;
+int *q;
+q = c;
 
-	printf("%d",number);
-	printf("%ls",&number);
 
-	
+
+printf("valor de a: %d \n", a);
+printf("valor de b: %d \n", b);
+printf("endereço de b: %p \n", &b);
+printf("valor de c: %p \n", c);
+printf("valor de q:%p\n",q);
 }
