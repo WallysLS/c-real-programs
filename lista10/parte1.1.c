fb01464c335de93786af6465ec287c54742498fd
WallysLS <wallys31.souza@gmail.com>
2022-07-07 22:47:16 -0300

    add important folders

---
+++ lista10/parte1.1.c
@@ -0,0 +1,14 @@
+//Implemente a  estrutura de dados em C que representa pontos em um espaço euclidiano
+
+struct Ponto {
+    float x = 2;
+    float y = 3;
+
+    
+}
+
+int main(){
+    float new = Ponto.x;
+    printf("%f", new);
+    return 0;
+}
